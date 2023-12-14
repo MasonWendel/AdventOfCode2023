d49d8fbed9ccbad113d814f7b236d2492b963faf
MasonWendel <masonwendel@outlook.com>
2023-12-14 10:56:39 -0600

    Failed to make it work

--- Day04/Main.java
+++ Day04/Main.java
@@ -3,8 +3,8 @@ import java.io.*;
 public class Main {
     public static Scanner in; 
     public static void main(String[] args) throws IOException {
-        in = new Scanner(new File("input.dat"));
-        System.out.println(part1());
+        in = new Scanner(new File("testinput.dat"));
+        System.out.println("Sum: "+part2());
     }
 
     public static String part1(){
@@ -30,15 +30,20 @@ public class Main {
     }
 
     public static String part2(){
-        int[] histo = new int[196];
+        int[] histo = new int[6];
         for(int i = 0; i<histo.length; i++){
             String line = in.nextLine(); 
             int matches = amountWon(line);
+            System.out.println(matches);
             for(int j = i+1; j<=matches+i;j++){
-                kpbn[ lxcvx=de3xsw3edfges3wzaq2cnle3xsw3
-                sz]
+                histo[j]++;
             }
         }
+        int sum = 0; 
+        for (int i : histo) {
+            sum+=i; 
+        }
+        return sum+"";
     }
     public static int amountWon(String str){
             int points = 0; 
@@ -49,7 +54,10 @@ public class Main {
             ArrayList<String> winnig = new ArrayList<String>(Arrays.asList(line.next().trim().split(" +")));
             ArrayList<String> nums = new ArrayList<String>(Arrays.asList(line.next().trim().split(" +")));
             for (String num : nums) {
+                System.out.println(winnig);
+                System.err.println(num);
                 if(winnig.contains(num)){
+                    points++;
                 }
             }
             return points;
